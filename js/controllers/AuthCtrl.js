86fb1c5d5884fb625106a65c955bf4b32d40eb97
TL Nguyen <tl.nguyen.1987@gmail.com>
2014-09-17 14:08:48 +0300

    login and regisration html forms + AuthCtrl initial template

---
+++ js/controllers/AuthCtrl.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+App.controller("LoginCtrl", ["$scope", "$http", "$location", "$route",
+        function ($scope, $http, $location, $route) {
+            
+            $scope.user = {};
+
+            $scope.login = function () {
+                
+            };
+        }
+    ])
+    .controller("RegisterCtrl", ["$scope", "$http", "$location", "$route",
+        function ($scope, $http, $location, $route) {
+
+            $scope.user = {};
+
+            $scope.checkUsernameAvailability = function () {
+
+            };
+
+            $scope.register = function () {
+
+            };
+        }
+    ]);
