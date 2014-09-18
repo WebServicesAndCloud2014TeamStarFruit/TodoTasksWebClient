31bc1b841004c5d4167048e8f117ac9acd1010e0
Ventsislav Georgiev <ventsislav.georgiev@live.com>
2014-09-18 08:40:59 +0300

    Use TypeScript for better intellisense. Added Notifications.

---
+++ js/controllers/AuthCtrl.ts
@@ -0,0 +1,65 @@
+﻿"use strict";
+
+App.controller("LoginCtrl", ["$scope", "$rootScope", "$http", "$location", "$route", "$window", "notificationService",
+	(
+		$scope: any,
+		$rootScope: any,
+		$http: ng.IHttpService,
+		$location: ng.ILocationService,
+		$route: ng.route.IRouteService,
+		$window: ng.IWindowService,
+		notification: INotificationService) => {
+		$scope.user = {};
+
+		$scope.login = () => {
+			$http.post('http://localhost:56841/Token', {
+				username: $scope.user.email,
+				password: $scope.user.password,
+				grant_type: "password"
+			}, {
+					transformRequest: obj => {
+						var str = [];
+						for (var p in obj)
+							str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+						return str.join("&");
+					},
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded'
+					}
+				})
+				.success(data => {
+					// authentication OK
+					$rootScope.user = data;
+					$rootScope.$emit('userChanged');
+					$location.url('/');
+					$window.sessionStorage.setItem("user", JSON.stringify(data));
+					console.log(data);
+				})
+				.error(errors => notification.addError(errors));
+		};
+	}
+])
+	.controller("RegisterCtrl", ["$scope", "$http", "notificationService",
+		(
+			$scope: any,
+			$http: ng.IHttpService,
+			notification: INotificationService) => {
+			$scope.user = {};
+
+			$scope.register = () => {
+				$http({
+					url: 'http://localhost:56841/api/Account/register',
+					method: "POST",
+					data: {
+						UserName: $scope.user.username,
+						Password: $scope.user.password,
+						ConfirmPassword: $scope.user.confirmPassword,
+						Email: $scope.user.email
+					},
+					withCredentials: false
+				})
+					.success(user => console.log(user))
+					.error(errors => notification.addError(errors));
+			};
+		}
+	]);
---
+++ js/controllers/TodoCtrl.ts
@@ -0,0 +1,70 @@
+﻿/// <reference path="../../scripts/typings/angularlocalstorage/angularlocalstorage.d.ts" />
+"use strict";
+
+App.controller("TodoCtrl",
+	(
+		$scope: any,
+		localStorageService: ng.localStorage.ILocalStorageService) => {
+		$scope.init = () => {
+			if (localStorageService.get("todoList") === null) {
+				$scope.model = [
+					{
+						name: "Primary", list: [
+							{ taskName: "Create an Angular-js TodoList", isDone: false },
+							{ taskName: "Understanding Angular-js Directives", isDone: true }
+						]
+					},
+					{
+						name: "Secondary", list: [
+							{ taskName: "Build an open-source website builder", isDone: false },
+							{ taskName: "BUild an Email Builder", isDone: false }
+						]
+					}
+				];
+			} else {
+				$scope.model = localStorageService.get("todoList");
+			}
+			$scope.show = "All";
+			$scope.currentShow = 0;
+		};
+
+		$scope.addTodo = () => {
+			/*Should prepend to array*/
+			$scope.model[$scope.currentShow].list.splice(0, 0, { taskName: $scope.newTodo, isDone: false });
+			/*Reset the Field*/
+			$scope.newTodo = "";
+		};
+
+		$scope.deleteTodo = (index) => {
+			$scope.model[$scope.currentShow].list.splice(index, 1);
+		};
+
+		$scope.todoSortable = {
+			containment: "parent",//Dont let the user drag outside the parent
+			cursor: "move",//Change the cursor icon on drag
+			tolerance: "pointer"//Read http://api.jqueryui.com/sortable/#option-tolerance
+		};
+
+		$scope.changeTodo = (i) => {
+			$scope.currentShow = i;
+		};
+
+		/* Filter Function for All | Incomplete | Complete */
+		$scope.showFn = todo => {
+			if ($scope.show === "All") {
+				return true;
+			} else if (todo.isDone && $scope.show === "Complete") {
+				return true;
+			} else if (!todo.isDone && $scope.show === "Incomplete") {
+				return true;
+			} else {
+				return false;
+			}
+		};
+
+		$scope.$watch("model", (newVal, oldVal) => {
+			if (newVal !== null && angular.isDefined(newVal) && newVal !== oldVal) {
+				localStorageService.set("todoList", angular.toJson(newVal));
+			}
+		}, true);
+	});
